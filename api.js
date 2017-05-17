5764470095bc6e4c993fc5a51410864846871242
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2017-05-17 03:24:08 +0300

    removed export folder

--- api.js
+++
@@ -1,7 +0,0 @@
-var sketchfileEditor = require('./sketchfile-editor');
-
-module.exports = function (app) {
-  app.get('/api/:width/:ratio/:baseline/:numberOfColumns/:gutter', function (req, res) {
-    sketchfileEditor(req.params, res);
-  });
-}
